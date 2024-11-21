650e490f0fae300c9049608512db6fc342889a75
RanaNilu <rana.nilu1@gmail.com>
2024-11-21 14:49:53 +0600

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,6 @@
+# api_keys.py
+import os
+
+GOOGLE_API_KEY = os.getenv("AIzaSyDiZjRdBVZNqmhCQHnqDjz_fjgdfARyZp4")
+TAVILY_API_KEY = os.getenv("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")
+OPENAI_API_KEY = os.getenv("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")
